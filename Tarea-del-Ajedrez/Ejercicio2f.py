a55ab728d2ddb0d858d3c7c0ddda457e181d2a05
mramirezcc <mramirezcc@unsa.edu.pe>
2024-05-25 15:50:12 -0500

    Ejercicio 'f' completo

--- Tarea-del-Ajedrez/Ejercicio2f.py
+++ Tarea-del-Ajedrez/Ejercicio2f.py
@@ -1,2 +1,7 @@
 from interpreter import draw
 from chessPictures import *
+pic = square.join(square.negative())
+pic = pic.horizontalRepeat(4)
+pic = pic.up(pic.negative())
+pic = pic.verticalRepeat(2)
+draw(pic)
