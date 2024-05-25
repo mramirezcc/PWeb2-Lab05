091c6f94ed598db7bcb18a652b5f905fd9686e7b
mramirezcc <mramirezcc@unsa.edu.pe>
2024-05-25 18:57:21 -0500

    Método mejorado(?). Manejo de errores de longitud y otros casos considerados

--- EjerciciosResueltos/matrizEscalar.py
+++ EjerciciosResueltos/matrizEscalar.py
@@ -1,21 +1,24 @@
 def esEscalar(m):
-    d = m[0][0]
-    for i in range(len(m)):
-        for j in range(len(m)):
-            if i != j:
-                if m[i][j] != 0:
-                    print(m[i][j])
+    try:
+        d = m[0][0]
+        for i in range(len(m)):
+            for j in range(len(m)):
+                if i != j:
+                    if m[i][j] != 0:
+                        print("Elemento incongruente: " + str(m[i][j]))
+                        return False
+                elif m[i][j] != d:
+                    print("Elemento incongruente:" + str(m[i][j]))
                     return False
-            elif m[i][j] != d:
-                print(m[i][j])
-                return False
+    except IndexError:
+        print("Las dimensiones de la matriz no son correctas")
+        return False
     return True
 
-matriz = [[5, 0, 0], [0, 5, 0], [0, 0, 5]]
-print("La matriz: ")
+matriz = [[5, 0, 0], [0, 5], [0, 0, 5]]
 for sub in matriz:
     print(sub)
 if(esEscalar(matriz)):
-    print("Es escalar")
+    print("La matriz es escalar")
 else:
-    print("No es escalar")
+    print("La matriz no es escalar")
