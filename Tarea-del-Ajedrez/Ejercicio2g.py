2ceeb3212293e8c3bddb1daff8d64f3fce015d63
mramirezcc <mramirezcc@unsa.edu.pe>
2024-05-25 17:39:31 -0500

    Ejercicio 'g' terminado

--- Tarea-del-Ajedrez/Ejercicio2g.py
+++ Tarea-del-Ajedrez/Ejercicio2g.py
@@ -1,9 +1,15 @@
 from interpreter import draw
 from chessPictures import *
 
+upper = rock.join(knight).join(bishop).join(queen).join(king).join(bishop).join(knight).join(rock)
+lower = pawn.horizontalRepeat(8)
+
 board = square.join(square.negative())
 board = board.horizontalRepeat(4)
 board = board.up(board.negative())
-board = board.verticalRepeat(4)
 
-draw(board)
+pic = board.under(upper.negative().up(lower.negative()))\
+    .up(board.verticalRepeat(2))\
+    .up(board.under(lower.up(upper)))
+
+draw(pic)
